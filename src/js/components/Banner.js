725ba5b540c671af7d0e044996c6e72b407d72d3
Yan Yu <yu.yan@rea-group.com>
2016-07-16 11:09:29 +0800

    test-1 - YY

--- src/js/components/Banner.js
+++ src/js/components/Banner.js
@@ -4,7 +4,9 @@ module.exports = React.createClass({
     render() {
         return (
             <div className="banner-merge">
-                <span className="icon-search"></span>
+                <span className="icon-search">1</span>
+                <span className="icon-search">2</span>
+                <span className="icon-search">3</span>
             </div>
         );
     }
