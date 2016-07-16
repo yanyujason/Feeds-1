b68d1e870059bdc17cd75da68de9c89ae40bd7a5
Yan Yu <yu.yan@rea-group.com>
2016-07-16 11:11:54 +0800

    uu

--- src/js/components/Banner.js
+++ src/js/components/Banner.js
@@ -3,10 +3,11 @@ import React from 'react';
 module.exports = React.createClass({
     render() {
         return (
-            <div className="banner-merge">
+            <div class="banner-merge">
                 <span className="icon-search">1</span>
                 <span className="icon-search">2</span>
                 <span className="icon-search">3</span>
+                <span className="icon-search">4</span>
             </div>
         );
     }
