ca10b7b24e48a77e5f87de7630a19fc77129facb
Yan Yu <yu.yan@rea-group.com>
2016-07-16 11:13:03 +0800

    conflicts

--- src/js/components/Banner.js
+++ src/js/components/Banner.js
@@ -3,7 +3,10 @@ import React from 'react';
 module.exports = React.createClass({
     render() {
         return (
-            <div className="banner">
+            <div className="banner12">
+                <span className="icon-search">2</span>
+                <span className="icon-search1"></span>
+                <span className="icon-search"></span>
                 <span className="icon-search"></span>
             </div>
         );
